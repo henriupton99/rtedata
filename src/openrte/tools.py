9b39d0918e1db569c629deaf7dbb8a8adc0fad54
henriupton99 <henriupton99@gmail.com>
2025-05-17 12:14:17 +0200

    resolve multiple analog prints in logger

--- src/openrte/tools.py
+++ src/openrte/tools.py
@@ -6,8 +6,9 @@ class Logger:
         self.level = logging.INFO
         self.logger = logging.getLogger(self.name)
         self.logger.setLevel(self.level)
-        formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
-
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+        
+        if not self.logger.handlers:
+            formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+            ch = logging.StreamHandler()
+            ch.setFormatter(formatter)
+            self.logger.addHandler(ch)
